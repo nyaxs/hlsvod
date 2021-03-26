c581167e1d04731e09c716218e4de38fc52320c4
nyaxs <1607746993@qq.com>
2021-03-26 16:18:01 +0800

    3

--- src/main/java/com/liuyh/hlsvod/M3U8Entity.java
+++ src/main/java/com/liuyh/hlsvod/M3U8Entity.java
@@ -5,10 +5,13 @@ import lombok.Data;
 
 @Data
 public class M3U8Entity {
+    //业务名，默认live,如app=live
     String app;
+    //直播码，如房间号3619101，或加上其他校验码，如stream=3619101?code=sdas
     String stream;
+    //直播的开始时间，如下午15点04分05秒，startTime=150405
     String startTime;
-    //String endTime;
+    //直播日期，如2006年01月02日，date=20060102
     String date;
 
 }
--- src/main/java/com/liuyh/hlsvod/M3u8Controller.java
+++ src/main/java/com/liuyh/hlsvod/M3u8Controller.java
@@ -4,6 +4,7 @@ import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.file.FileWriter;
 import cn.hutool.core.util.StrUtil;
 import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,28 +17,33 @@ import java.util.List;
 
 import static cn.hutool.core.util.StrUtil.*;
 
+@Slf4j
 @RestController
 public class M3u8Controller {
     @GetMapping("/m3u8")
     public String getM3u8(@RequestBody M3U8Entity m3U8Entity) {
         //时间格式应为[2006][01][02]/[15][04][05].ts;，
         // 例如2006年01月02日15点04分05秒，startTime = "150405",date = "20060102"
+        log.info("----传入参数值为----{}",m3U8Entity);
         String app = m3U8Entity.getApp();
         String stream = m3U8Entity.getStream();
         String startTime = m3U8Entity.getStartTime();
         String date = m3U8Entity.getDate();
+        //需要补充参数合法性校验
+        //if(valid) do
+        String fullTime = date+startTime;
         String srsHttpBasePath = "/home/liuyh/srs/trunk/objs/nginx/html/";
         String hlsvodSavePath = "/home/liuyh/vod/";
         String tsFilePath = "/home/liuyh/srs/trunk/objs/nginx/html/"+app+C_SLASH+stream+C_SLASH+date+C_SLASH;
-        String tsFrom = srsHttpBasePath+app+C_SLASH+stream;
-        String tsTo = hlsvodSavePath+app+C_SLASH+stream+C_SLASH+date+startTime;
-        String tsDir = stream+C_SLASH+date+C_SLASH;
+        String tsFrom = srsHttpBasePath+app+C_SLASH+stream+C_SLASH+date;
+        String tsTo = hlsvodSavePath+app+C_SLASH+stream+C_SLASH;
+        String tsDir = fullTime+C_SLASH+date+C_SLASH;
         String m3u8Start = "#EXTM3U" + C_LF +
                 "#EXT-X-PLAYLIST-TYPE:VOD" + C_LF +
                 "#EXT-X-TARGETDURATION:10\n";
         String m3u8End = "#EXT-X-ENDLIST";
         String tsDesc = "#EXTINF:1.000, no desc\n";
-        String ts = "150740.ts";
+        String ts = "150405.ts";
         StringBuilder sbContent = StrUtil.builder(ts.length() * 1800);
         sbContent.append(m3u8Start);
         String tsTemp = "123456";
@@ -59,16 +65,19 @@ public class M3u8Controller {
             }
         }
         sbContent.append(m3u8End);
-        if (!FileUtil.exist(app)){
-            FileUtil.copy(tsFrom,tsTo,true);
+
+        if (!FileUtil.exist(tsTo+fullTime)){
+
+            FileUtil.copy(tsFrom,tsTo+fullTime,true);
         }else {
-            FileUtil.copy(tsFrom,tsTo,true);
+            System.out.println(tsTo+fullTime+"-----该目录已存在");
+            FileUtil.copy(tsFrom,tsTo+fullTime,false);
         }
         //拼写 m3u8内容的 sbContent写入文件
-        FileWriter writer = new FileWriter(tsTo+C_SLASH+stream+"-"+date+startTime+".m3u8");
+        FileWriter writer = new FileWriter(tsTo+C_SLASH+stream+"-"+fullTime+".m3u8");
         writer.write(sbContent.toString());
         //返回 m3u8 文件 http地址
-        return "http://172.31.234.199:8080/vod/"+app+C_SLASH+stream+"-"+date+startTime+".m3u8";
+        return "http://172.31.234.199:8080/"+app+C_SLASH+stream+C_SLASH+stream+"-"+fullTime+".m3u8";
     }
 
     @GetMapping("/test")
@@ -112,6 +121,7 @@ public class M3u8Controller {
         if (!FileUtil.exist(app)){
             FileUtil.copy(tsFrom,tsTo+fullTime,true);
         }else {
+
             FileUtil.copy(tsFrom,tsTo+fullTime,true);
         }
         //拼写 m3u8内容的 sbContent写入文件
